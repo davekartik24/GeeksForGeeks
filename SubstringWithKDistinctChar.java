c93b88f786ee0ab4aa42f3281121f5bf8b8d79ee
Kartik Dave <davekartik24@gmail.com>
2019-02-24 23:12:22 -0800

    Solution for all substrings with K distinct characters

--- SubstringWithKDistinctChar.java
+++ SubstringWithKDistinctChar.java
@@ -1,9 +1,46 @@
 
+import java.util.*;
 
 public class SubstringWithKDistinctChar{
 
+
+	public int substringWithKDistinctChar(String inputString, int k) {
+
+		int i = 0;
+		int j = 0;
+
+		int n = inputString.length();
+
+		int result = 0;
+
+		HashSet<Character> lookup = new HashSet<>();
+
+		while(i <= (n - k)) {
+			
+			if(lookup.size() < k) {
+				lookup.add(inputString.charAt(j++));
+			} else if(j < n && lookup.size() == k) {
+				result++;
+				lookup.add(inputString.charAt(j++));
+			} else {
+				if(lookup.size() == k) {
+					result++;
+				}
+				lookup.clear();
+
+				j = ++i;
+			}
+		}
+
+		return result;
+
+	}
+
 	public static void main(String[] args) {
 
-		
+		SubstringWithKDistinctChar testObject = new SubstringWithKDistinctChar();
+
+		System.out.println(testObject.substringWithKDistinctChar("aa", 1));
+
 	}
 }
